a07c8e797a502c0438f8b63d5b575021b0a8a387
gvipul-294 <gvipul294@gmail.com>
2020-07-12 22:04:26 +0530

    Changes to be committed: Implemented different bicycle type features with the help of interfaces Methods in different scenarios are constructed according to specialised situation

--- SourceCode/MyProject.java
+++ SourceCode/MyProject.java
@@ -1,8 +1,14 @@
 package learnOOAD;
 
+interface operations{
+    
+    double ratio();
+    double diameter();
+    double gear_inches();
 
+}
 
-class Gear{
+class bicycleGear implements operations{
 
     static class wheel{
 
@@ -56,12 +62,12 @@ class Gear{
 
     private data mydata;
 
-    Gear(int chainring, int cog){
+    bicycleGear(int chainring, int cog){
         
         mydata=new data(chainring,cog);
 
     }
-    Gear(int chainring, int cog, int rim, double tire){
+    bicycleGear(int chainring, int cog, int rim, double tire){
         
         mydata=new data(chainring,cog,rim,tire);
 
@@ -82,13 +88,94 @@ class Gear{
 
 }
 
+class superCycleGear implements operations{
+
+    static class wheel{
+
+        private int rim;
+        private double tire;
+
+        wheel(int rim, double tire){
+            this.rim=rim;
+            this.tire=tire;
+        }
+
+        private int getRim(){
+            return this.rim;
+        }
+        private double getTire(){
+            return this.tire;
+        }
+    }
+
+    static class data{
+        private int chainring;
+        private int cog;
+        
+        private wheel wheeldata;
+
+        data(int chainring, int cog){
+            this.chainring=chainring;
+            this.cog=cog;
+    
+        }
+        data(int chainring, int cog, int rim, double tire){
+            this.chainring=chainring;
+            this.cog=cog;
+            wheeldata=new wheel(rim,tire);
+        }
+
+        private int getChainring(){
+            return this.chainring;
+        }
+        private int getCog(){
+            return this.cog;
+        }
+        private int getRim(){
+            return wheeldata.getRim();
+        }
+        private double getTire(){
+            return wheeldata.getTire();
+        }
+
+    }
+
+    private data mydata;
+
+    superCycleGear(int chainring, int cog){
+        
+        mydata=new data(chainring,cog);
+
+    }
+    superCycleGear(int chainring, int cog, int rim, double tire){
+        
+        mydata=new data(chainring,cog,rim,tire);
+
+    }
+
+    public double ratio(){
+        return (mydata.getChainring()*(double)2)/(double)mydata.getCog();
+    }
+
+    public double diameter(){
+        return ((double)mydata.getRim()+mydata.getTire()*((double)3));
+    }
+    public double gear_inches(){
+        return ratio()*diameter()*2;
+    }
+
+
+
+}
+
+
 public class MyProject {
 
     public static void WorkingWithGearClass() {
-        Gear mygear=new Gear(52,11,26,1.5);
+        operations mygear=new bicycleGear(52,11,26,1.5);
         System.out.println(mygear.gear_inches());
 
-        Gear mygear2=new Gear(52,11,24,1.25);
+        operations mygear2=new superCycleGear(52,11,24,1.25);
         System.out.println(mygear2.gear_inches());
 
         
