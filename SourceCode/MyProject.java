42e872dde0dedf4778f6518b565f3c2af15e71a0
gvipul-294 <gvipul294@gmail.com>
2020-07-16 10:05:18 +0530

    Changes to be committed:
    Implemented the Parts Class as a separate Class component
    Implemented the Sub Class of Parts as the Super Class
    Designed the specific functionality of each sub class inheriting from Parts Component
    Implemented the bicycle class with the help of composing the Parts Object

--- SourceCode/MyProject.java
+++ SourceCode/MyProject.java
@@ -168,6 +168,134 @@ class superCycleGear implements operations{
 
 }
 
+class Parts{
+
+    private String chain;
+    private int tire_size;
+
+    Parts(String chain, int tire_size){
+        this.chain=chain;
+        this.tire_size=tire_size;
+    }
+
+    public String getChain(){
+        return this.chain;
+    }
+
+    public int getTire_size(){
+        return this.tire_size;
+    }
+
+     
+
+    public String toString() {
+        StringBuilder result=new StringBuilder();
+        result.append("\nSpare Parts: ");
+        result.append("\nchain: "+getChain());
+        result.append("\ntire_size: "+getTire_size());
+        return result.toString();
+    }
+  
+}
+
+class RoadBikersParts extends Parts{
+
+    private String tape_color;
+    
+    RoadBikersParts(String chain, int tire_size, String tape_color){
+        super(chain,tire_size);
+        tape_color=this.tape_color;
+    }
+
+    RoadBikersParts(String chain, String tape_color){
+        super(chain,23);
+        tape_color=this.tape_color;
+    }
+
+    public String getTape_color(){
+        return this.tape_color;
+    }
+
+    public String toString() {
+        StringBuilder result=new StringBuilder();
+        result.append("\nSpare Parts: ");
+        result.append("\nchain: "+getChain());
+        result.append("\ntire_size: "+getTire_size());
+        result.append("\ntape_color: "+getTape_color());
+        return result.toString();
+    }
+
+}
+
+class MountainBikersParts extends Parts{
+
+    private String front_shock;
+    private String back_shock;
+    
+    MountainBikersParts(String chain, int tire_size, String front_shock, String back_shock){
+        super(chain,tire_size);
+        this.front_shock=front_shock;
+        this.back_shock=back_shock;
+    }
+
+    MountainBikersParts(String chain, String front_shock, String back_shock){
+        super(chain,21);
+        this.front_shock=front_shock;
+        this.back_shock=back_shock;
+    }
+
+    public String getFront_Shock(){
+        return this.front_shock;
+    }
+    public String getBack_Shock(){
+        return this.back_shock;
+    }
+
+
+    public String toString() {
+        StringBuilder result=new StringBuilder();
+        result.append("\nSpare Parts: ");
+        result.append("\nchain: "+getChain());
+        result.append("\ntire_size: "+getTire_size());
+        result.append("\nfront shock: "+getFront_Shock());
+        result.append("\nback shock: "+getBack_Shock());
+        return result.toString();
+    }
+
+}
+
+class bicycle{
+
+    private int size;
+    
+    private Parts myParts;
+
+    bicycle(int size, String chain, int tire_size, String tape_color){
+        this.size=size;
+        myParts=new RoadBikersParts(chain, tire_size, tape_color);
+    }
+
+    bicycle(int size, String chain, int tire_size, String front_shock, String back_shock){
+        this.size=size;
+        myParts=new MountainBikersParts(chain, tire_size, front_shock, back_shock);
+    }
+
+    public int getSize(){
+        return this.size;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result=new StringBuilder();
+        result.append("Size: "+ size);
+        result.append(myParts.toString());
+        return result.toString();
+        
+    }
+
+
+
+}
 
 public class MyProject {
 
@@ -178,7 +306,9 @@ public class MyProject {
         operations mygear2=new superCycleGear(52,11,24,1.25);
         System.out.println(mygear2.gear_inches());
 
-        
+        bicycle obj=new bicycle(4, "chain", 4, "fox", "back_shock");
+
+        System.out.println(obj);
     }
 
     
